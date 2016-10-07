299a6b66413b154fba6976418f3f3b5a41fe3424
Marcelo <martimiano.marcelo@gmail.com>
2016-10-06 23:22:54 -0300

    Codigo para a apresentacao, motores e ultrassonico frontal funcionando

--- GERaldo/Codigo_GERaldo/Codigo_GERaldo.ino
+++ GERaldo/Codigo_GERaldo/Codigo_GERaldo.ino
@@ -1,3 +1,5 @@
+#include <NewPing.h>
+
 /* Codigo fonte do robo GERaldo para o TJR-2016.
  * Autores: 
  *          Leonardo Alves de Melo
@@ -16,16 +18,46 @@
 
 #define WAIT 5000     //Constante de tempo
 #define VALOR_PWM 255 //Valor
-#define TEMPO_DE_GIRO 2000
+#define TEMPO_DE_GIRO 1000 // aprox 90 graus
+
+// Sensor Frontal
+#define TRIG_F 3
+#define ECHO_F 4
+
+// Sensor Direito
+//#define TRIG_D 
+//#define ECHO_D 
+
+#define MAX_DIST 50
+
+NewPing sensor_frontal(TRIG_F, ECHO_F, MAX_DIST);
+//NewPing sensor_direito(TRIG_D, ECHO_D, MAX_DIST);
 
 void setup()
 {
   inicializa_motores();
+  inicializa_sensores();
+  
+  Serial.begin(115200);
 }
 
 void loop()
 {
-  frente(); 
+  Serial.println(sensor_frontal.ping_cm());
+  int d = sensor_frontal.ping_cm();
+
+  if (d < MAX_DIST && d > 0)
+  {
+    while(true)
+    {
+      frente();
+    }
+  } else
+  {
+    direita();
+  }
+  
+  delay(10);
 }
 
 void inicializa_motores()
@@ -40,6 +72,11 @@ void inicializa_motores()
 
   analogWrite (PWM_1, VALOR_PWM);
   analogWrite (PWM_2, VALOR_PWM);
+}
+
+void inicializa_sensores()
+{
+ 
 }
 
 void frente()
@@ -80,3 +117,14 @@ void direita()
   
   delay(TEMPO_DE_GIRO);
 }
+
+void freia()
+{
+  digitalWrite(MOTOR_1A, LOW);
+  digitalWrite(MOTOR_1B, LOW);
+  
+  digitalWrite(MOTOR_2A, LOW); 
+  digitalWrite(MOTOR_2B, LOW);
+  
+}
+  
